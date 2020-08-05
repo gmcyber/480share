7fb97aa49f9c138cfb1d3077ed14d5cb75d3b3f3
gmcyber <37155987+gmcyber@users.noreply.github.com>
2020-08-05 11:36:10 -0400

    Create ubuntu-sealer.sh

---
+++ ubuntu-sealer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#script to prepare ubuntu vm for cloning
+apt-get dist-update
+apt-get install open-vm-tools-desktop openssh-server
+cat /dev/null > /var/log/wtmp 
+cat /dev/null > /var/log/lastlog 
+rm -rf /tmp/*
+rm -rf /var/tmp/*
+rm -f /etc/ssh/ssh_host*
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+cat <<EOL | sudo tee /etc/rc.local
+#!/bin/sh -e
+test -f /etc/ssh/ssh_host_dsa_key || dpkg-reconfigure openssh-server
+exit 0
+EOL
+echo ubuntu > /etc/hostname
+apt-get clean
+history -c
+history -w
+chmod +x /etc/rc.local
+systemctl stop apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.timer
+systemctl stop apt-daily.timer
+systemctl disable apt-daily.timer
+echo "remove git repo and then issue a shutdown - h now"
